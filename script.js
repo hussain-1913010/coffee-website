83fbc979a62e88bd4a77079b204a2973730e3cf6
hussain-1913010 <hussain1913010@stud.kuet.ac.bd>
2021-10-30 03:39:23 +0600

    heading part complete with responsive testing

---
+++ script.js
@@ -0,0 +1,71 @@
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menuBtn').onclick = () => {
+    navbar.classList.toggle('active');
+    
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+}
+
+
+
+let searchForm = document.querySelector('.searchForm');
+
+document.querySelector('#searchBtn').onclick = () => {
+    searchForm.classList.toggle('active');
+
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+}
+
+
+
+
+let cartItem = document.querySelector('.cartItemsContainer');
+
+document.querySelector('#cartBtn').onclick = () => {
+    cartItem.classList.toggle('active');
+
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+}
+
+
+
+
+
+window.onscroll = () => {
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
